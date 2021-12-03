c733704a6df3df365fa8d01f5690f9080ebb384c
mlt-afrez <55949370+mlt-afrez@users.noreply.github.com>
2021-12-03 05:33:43 +0300

    MLM first commit

---
+++ mlm_marketing/models/partner.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+
+
+class Partner(models.Model):
+    _inherit = 'res.partner'
+
+    user_id = fields.Many2one('res.users', string='Salesperson', index=True, tracking=True,
+                              default=lambda self: self.env.user)
