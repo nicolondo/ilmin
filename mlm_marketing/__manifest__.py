e70eee67ad8d52eb869e8e50e10798895005ebc1
mlt-afrez <55949370+mlt-afrez@users.noreply.github.com>
2021-12-06 01:38:30 +0300

    pos restriction

--- mlm_marketing/__manifest__.py
+++ mlm_marketing/__manifest__.py
@@ -20,13 +20,12 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','point_of_sale'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'views/pos.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
