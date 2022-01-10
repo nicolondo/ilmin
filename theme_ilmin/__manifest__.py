a4fe8a90e1e3ad54dfa2a2df861bf54f7f76ebb3
mlt-afrez <55949370+mlt-afrez@users.noreply.github.com>
2022-01-10 12:39:35 +0300

    update view

--- theme_ilmin/__manifest__.py
+++ theme_ilmin/__manifest__.py
@@ -27,7 +27,7 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-        'static/src/xml/website_template.xml',
+        'views/website_template.xml',
 
     ],
     # only loaded in demonstration mode
